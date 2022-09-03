8da38c62b20b166c08b97477fa59ab51d5edfa58
hh9748 <59165072+hh9748@users.noreply.github.com>
2022-09-03 18:35:24 +0800

    将java工程添加到版本库

---
+++ day01/src/com/softeem/demo/Hello.java
@@ -0,0 +1,13 @@
+package com.softeem.demo;
+
+/**
+ * Hello class
+ *
+ * @author Huanghao
+ * @date 2022/03/28
+ */
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("Hello World !你好世界");
+    }
+}
---
+++ day01/src/com/softeem/demo/TestDemo1.java
@@ -0,0 +1,20 @@
+package com.softeem.demo;
+
+/**
+ * TestDemo1 class
+ * 输出：
+ *  编号 姓名 班级 成绩
+ * 111 张三 计科一班 88
+ * 222 李四 计科二班 90
+ * 333 王五 计科一班 75
+ * @author Huanghao
+ * @date 2022/03/28
+ */
+public class TestDemo1 {
+    public static void main(String[] args) {
+        System.out.println("编号\t姓名\t班级\t成绩");
+        System.out.println("111\t张三\t计科一班\t88");
+        System.out.println("222\t李四\t计科二班\t90");
+        System.out.println("333\t王五\t计科一班\t75");
+    }
+}
